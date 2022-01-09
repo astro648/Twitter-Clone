644d3b0eb2de3e129ff9c71f518387c0c4701599
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2022-01-09 10:34:52 -0800

    send msg + backend change

--- kwitter_page.js
+++ kwitter_page.js
@@ -11,7 +11,17 @@ var firebaseConfig = {
     
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-user_name = localstorage.getItem("user_name");
+user_name = localStorage.getItem("user_name");
+room_name = localStorage.getItem("room_name");
+function send(){
+      msg = document.getElementById("msg").value;
+      firebase.database().ref(room_name).push({
+            name: user_name,
+            message: msg,
+            like: 0            
+      });
+      document.getElementById("msg").value = "";
+}
 function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
          firebase_message_id = childKey;
          message_data = childData;
