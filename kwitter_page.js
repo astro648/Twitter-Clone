8ece34f7d8188d3d9d5ac2a31c0bb9302a8b5147
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2022-01-09 10:28:30 -0800

    firebase init

--- kwitter_page.js
+++ kwitter_page.js
@@ -1,4 +1,16 @@
 //YOUR FIREBASE LINKS
+var firebaseConfig = {
+      apiKey: "AIzaSyCXzYWuadEf-KxET5KebXi3uXTgSdaM3VM",
+      authDomain: "kwitter-caad5.firebaseapp.com",
+      databaseURL: "https://kwitter-caad5-default-rtdb.firebaseio.com",
+      projectId: "kwitter-caad5",
+      storageBucket: "kwitter-caad5.appspot.com",
+      messagingSenderId: "827196602785",
+      appId: "1:827196602785:web:2b8933be6c7326b9da33e0"
+    };
+    
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
          firebase_message_id = childKey;
