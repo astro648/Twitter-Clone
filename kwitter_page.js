acf157f061e7501cce78580b9f11bd02f7943154
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2022-01-09 10:29:43 -0800

    some changes

--- kwitter_page.js
+++ kwitter_page.js
@@ -11,7 +11,7 @@ var firebaseConfig = {
     
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-
+user_name = localstorage.getItem("user_name");
 function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
          firebase_message_id = childKey;
          message_data = childData;
